71ab2eb4c59d96361648d2a387c0e7171cd6cca3
h662 <h662hong@gmail.com>
2025-03-05 15:26:42 +0900

    Fin Contracts Part

--- contracts/LiquidityPool.sol
+++ contracts/LiquidityPool.sol
@@ -26,4 +26,55 @@ contract LiquidityPool is ERC20 {
         uint liquidity = amountA + amountB;
         _mint(msg.sender, liquidity);
     }
+
+    function removeLiquidity(uint liquidity) external {
+        require(balanceOf(msg.sender) >= liquidity, "Not enough LP tokens");
+
+        uint totalLPTokens = totalSupply();
+
+        uint amountA = (liquidity * reserveA) / totalLPTokens;
+        uint amountB = (liquidity * reserveB) / totalLPTokens;
+
+        _burn(msg.sender, liquidity);
+        reserveA -= amountA;
+        reserveB -= amountB;
+
+        require(tokenA.transfer(msg.sender, amountA), "Failed Transfer TokenA");
+        require(tokenB.transfer(msg.sender, amountB), "Failed Transfer TokenB");
+    }
+
+    function getInputPrice(uint inputAmount, uint inputReserve, uint outputReserve) public pure returns (uint) {
+        require(inputReserve > 0 && outputReserve > 0, "Error Reserve Value");
+
+        uint inputAmoutWithFee = inputAmount * 997; // 0.3% 수수료 적용 (1000 - 3)
+
+        uint numerator = inputAmoutWithFee * outputReserve;
+        uint denominator = (inputReserve * 1000) + inputAmoutWithFee;
+
+        return numerator / denominator;
+    }
+
+    function swapAForB(uint amountAIn, uint minBOut) external {
+        require(tokenA.transferFrom(msg.sender, address(this), amountAIn), "Failed Transfer TokenA");
+
+        uint tokenBOut = getInputPrice(amountAIn, reserveA, reserveB);
+        require(tokenBOut >= minBOut, "Not Enough TokenB");
+
+        reserveA += amountAIn;
+        reserveB -= tokenBOut;
+
+        require(tokenB.transfer(msg.sender, tokenBOut), "Failed Transfer TokenB");
+    }
+
+    function swapBForA(uint amountBIn, uint minAOut) external {
+        require(tokenB.transferFrom(msg.sender, address(this), amountBIn), "Failed Transfer TokenB");
+
+        uint tokenAOut = getInputPrice(amountBIn, reserveB, reserveA);
+        require(tokenAOut >= minAOut, "Not Enough TokenB");
+
+        reserveB += amountBIn;
+        reserveA -= tokenAOut;
+
+        require(tokenA.transfer(msg.sender, tokenAOut), "Failed Transfer TokenA");
+    }
 }
